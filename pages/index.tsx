cfaaf5c619545d67aa11d4c58decada8694e7d82
kimdongzzi <dongzzi2035@gmail.com>
2023-02-01 13:15:34 +0900

    [feat]:practice tailwind css #1
    - Tailwind Css Set up & practice

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ const Home: NextPage = () => {
               <span className="text-sm text-gray-500">Orders</span>
               <span className="font-medium">340</span>
             </div>
-            <div className="h-24 w-24 rounded-full bg-zinc-300 group-hover:bg-red-300 transition-colors" />
+            <div className="h-24 w-24 rounded-full bg-zinc-300 group-hover:bg-red-300 transition" />
             <div className="flex flex-col items-center">
               <span className="text-sm text-gray-500">Spent</span>
               <span className="font-medium">$2,310</span>
