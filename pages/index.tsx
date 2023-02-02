b0490526b617de37c2eaae76bddef79b213d65b8
kimdongzzi <dongzzi2035@gmail.com>
2023-02-02 16:58:41 +0900

    [init]feat/UI: create branch #2
    - 캐럿마켓 클론코딩 UI 브랜치 생성

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,7 @@
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return (
-    <div className="bg-[url('/vercel.svg')]">
-      <h2 className="text-[200px] text-[#f98123]">Hello</h2>
-    </div>
-  );
+  return null;
 };
 
 export default Home;
