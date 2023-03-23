234b224c289c59d434a922cf3342c3833710c003
kimdongzzi <dongzzi2035@gmail.com>
2023-03-23 10:08:41 +0900

    [Prac]feat/react-hook-form: Prac React-Hook-Form #20
    - react-hook-form 사용법을 연습합니다.

---
+++ pages/form.tsx
@@ -0,0 +1,75 @@
+import { FieldErrors, useForm } from "react-hook-form";
+
+// less code (c)
+// better validation (c)
+// better errors (sset, clear, display)
+// have control over inputs
+// dont deal with events (c)
+// easier inputs (c)
+
+interface LoginForm {
+  username: string;
+  password: string;
+  email: string;
+  errors?: string;
+}
+
+export default function Forms() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+    setError,
+    reset,
+  } = useForm<LoginForm>({
+    mode: "onChange",
+  });
+
+  const onValid = (data: LoginForm) => {
+    console.log("data : ", data);
+    reset();
+  };
+
+  const onInvalid = (errors: FieldErrors) => {
+    console.log(errors);
+  };
+
+  return (
+    <form onSubmit={handleSubmit(onValid, onInvalid)}>
+      <input
+        {...register("username", {
+          required: "Username is required",
+          minLength: {
+            message: "The username should be longer than 5 chars",
+            value: 5,
+          },
+        })}
+        type="text"
+        placeholder="Username"
+      />
+      {errors.username?.message}
+      <input
+        {...register("email", {
+          required: "Email is required",
+          validate: {
+            notGmail: (value) =>
+              !value.includes("@gmail.com") || "Gmail is not allowed",
+          },
+        })}
+        type="email"
+        placeholder="Email"
+        className={`${Boolean(errors.email) ? "border border-red-500" : " "}`}
+      />
+      <span>{errors.email?.message}</span>
+      <input
+        {...register("password", {
+          required: "Password is required",
+        })}
+        type="password"
+        placeholder="Password"
+      />
+      <input type="submit" value="Create Account" />
+      {errors.errors?.message}
+    </form>
+  );
+}
